143d1ff1f084b2d30311d9287225eb5de73bc917
Chandra Kiran <chandu22404@gmail.com>
2026-02-23 17:24:31 +0530

    even or odd

---
+++ 2.java
@@ -0,0 +1,16 @@
+class B
+{
+	static int a=10;
+	public static void main(String [] args)
+	{
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+	}
+}
